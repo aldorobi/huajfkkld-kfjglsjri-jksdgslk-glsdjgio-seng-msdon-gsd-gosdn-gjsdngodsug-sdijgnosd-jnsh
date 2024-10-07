8d8f7da4a447e4440cdc662245654599c773981f
aldorobi <97127948+aldorobi@users.noreply.github.com>
2024-10-07 15:05:18 -0600

    Add files via upload

---
+++ PRO_C161_AM3_V2-main/shoot.js
@@ -0,0 +1,69 @@
+AFRAME.registerComponent("bullets", {
+  init: function () {
+    this.shootBullet();
+  },
+  shootBullet: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "z") {
+        var bullet = document.createElement("a-entity");
+
+        bullet.setAttribute("geometry", {
+          primitive: "sphere",
+          radius: 0.1,
+        });
+
+        bullet.setAttribute("material", "color", "black");
+
+        var cam = document.querySelector("#camera");
+
+        pos = cam.getAttribute("position");
+
+        bullet.setAttribute("position", {
+          x: pos.x,
+          y: pos.y,
+          z: pos.z,
+        });
+
+        var camera = document.querySelector("#camera").object3D;
+
+        // Establecer la dirección de la cámara como un vector de Three.js
+        var direction = new THREE.Vector3();
+        camera.getWorldDirection(direction);
+
+        // Establecer la velocidad y su dirección
+        bullet.setAttribute("velocity", direction.multiplyScalar(-10));
+
+        var scene = document.querySelector("#scene");
+        bullet.setAttribute('dynamic-body',{
+          shape: 'sphere',
+          mass : '0',
+        });
+        bullet.addEventListener('collide',this.removeBullet);
+
+        scene.appendChild(bullet);
+      }
+    });
+  },
+  removeBullet : function(e){
+    console.log(e.detail.target.el);
+    console.log(e.detail.body.el);
+    var element = e.detail.target.el;
+    var elementHit = e.detail.body.el;
+    if (elementHit.id.includes('box')){
+      elementHit.setAttribute('material',{
+        opacity : 1 ,
+        transparent : true ,
+
+      });
+      var impulse = new CANNON.Vec3(-2,2,1);
+      var worldPoint = new CANNON.Vec3().copy(elementHit.getAttribute('position'));
+      elementHit.body.apply(impulse,worldPoint);
+      element.removeEventListener('collide',this.shoot);
+      var scene = document.querySelector('#scene');
+      scene.removeChild(element);
+    }  
+
+  }
+});
+
+
